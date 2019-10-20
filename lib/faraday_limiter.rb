92e7809b0f903255f9f63f970d316c2851c9b35a
yann marquet <ymarquet@gmail.com>
2019-10-20 01:21:38 +0100

    decent implementation

--- lib/faraday_limiter.rb
+++ lib/faraday_limiter.rb
@@ -2,6 +2,7 @@ require 'singleton'
 require 'concurrent-edge'
 require 'faraday_limiter/version'
 require 'faraday_limiter/bucket'
+require 'faraday_limiter/redis_bucket'
 require 'faraday'
 
 module FaradayLimiter
@@ -14,28 +15,36 @@ module FaradayLimiter
 
     def initialize(app, options = {})
       super(app)
-      self.app     = app
-      self.options = options
-      self.buckets = Concurrent::LazyRegister.new
-      bucket_ids = options.fetch(:bucket_ids) { [DEFAULT_BUCKET_KEY] }
-
-      limit_per_bucket = limit / bucket_ids.size
-
-      bucket_ids.each do |bucket_id|
-        buckets.add(bucket_id) { Bucket.new(limit_per_bucket, interval) }
-      end
+      self.app        = app
+      self.options    = options
+      self.bucket_ids = options.fetch(:bucket_ids) { [DEFAULT_BUCKET_KEY] }
+      raise ArgumentError if limit < bucket_ids.size
     end
 
     def call(env)
-      bucket_key   = env.request.context[:bucket_key] || DEFAULT_BUCKET_KEY
+      bucket_id   = env.request.context[:bucket_id] || DEFAULT_BUCKET_KEY
       request_cost = env.request.context && env.request.context[:request_cost] || 1
 
-      buckets[bucket_key].take(request_cost) { app.call(env) }
+      buckets[bucket_id].take(request_cost) { app.call(env) }
     end
 
     private
 
-    attr_accessor :app, :options, :buckets
+    attr_accessor :app, :options, :bucket_ids
+
+    def buckets
+      @buckets ||= begin
+        register         = Concurrent::LazyRegister.new
+        limit_per_bucket = (limit / bucket_ids.size.to_f).ceil
+        tokens = Array.new(limit) { |i| i }
+
+        bucket_ids.each do |bucket_id|
+          bucket_limit = tokens.pop(limit_per_bucket).size
+          register.add(bucket_id) { RedisBucket.new(bucket_id, bucket_limit, resets_at: (Time.now + interval).to_i) }
+        end
+        register
+      end
+    end
 
     def limit
       @limit ||= options[:limit]
